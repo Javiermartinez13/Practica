4d616468063b2111a3a72d507560e6ae9fb7bc78
Javiermartinez13 <franciscojavier.martinezfernan@usp.ceu.es>
2024-02-14 09:14:51 +0100

    entrega1

--- src/mates/Matematicas.java
+++ src/mates/Matematicas.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class Matematicas {
 
-    public static double generarNumeroPiIterativo(long pasos) {
+    public static double generarNumeroPi(long pasos) {
         long puntosEnCirculo = 0;
 
         Random random = new Random();
@@ -13,16 +13,16 @@ public class Matematicas {
             double x = random.nextDouble();  
             double y = random.nextDouble();  
 
-            // Calcular la distancia desde el punto al centro (0.5, 0.5)
+            
             double distanciaAlCentro = Math.sqrt(Math.pow(x - 0.5, 2) + Math.pow(y - 0.5, 2));
 
-            // Verificar si el punto está dentro del círculo (radio = 0.5)
+            
             if (distanciaAlCentro <= 0.5) {
                 puntosEnCirculo++;
             }
         }
 
-        // Calcular la aproximación de pi utilizando la relación puntosEnCirculo / totalPuntos
+        
         double aproximacionPi = 4.0 * puntosEnCirculo / pasos;
 
         return aproximacionPi;
@@ -30,7 +30,7 @@ public class Matematicas {
 
     public static void main(String[] args) {
         long pasos = 100000;  // Puedes ajustar este valor según sea necesario
-        double aproximacionPi = generarNumeroPiIterativo(pasos);
+        double aproximacionPi = generarNumeroPi(pasos);
         System.out.println("Aproximación de pi después de " + pasos + " pasos: " + aproximacionPi);
     }
 }
